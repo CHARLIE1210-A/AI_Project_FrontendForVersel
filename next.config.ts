ae72566739e2ad986044d8788919d64b071c2ffc
Ashutosh <ash04singh@gmail.com>
2025-04-06 04:48:17 +0530

    Fix:versel error 2

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,16 @@
+// next.config.ts
 import type { NextConfig } from "next";
-import { Configuration } from 'webpack';
+import { Configuration } from "webpack";
 
-const nextConfig: { webpack: (config: Configuration) => Configuration } = {
-  webpack(config) {
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  webpack(config: Configuration) {
     config.module?.rules?.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
-      eslint: {
-        ignoreDuringBuilds: true,
-      },
     });
     return config;
   },
